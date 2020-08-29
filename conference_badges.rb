f3f4901094ee29b8b0800b13cc8e5c42598d312d
Anthony Hernandez <anthonyh2020@gmail.com>
2020-08-29 00:05:58 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,9 +2,9 @@ def badge_maker(name)
   return "Hello, my name is #{name}."
 end
 
-def batch_badge_creator(speakers)
+def batch_badge_creator(guests)
   badges = []
-  speakers.each do |badge| 
+  guests.each do |badge| 
   badges.push("Hello, my name is #{badge}")
 end
   return badges
